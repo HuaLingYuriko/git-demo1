7dbd7c04bbdc09d1a13571cf4ebaeb6eff9ac71c
baihezi <973227960@qq.com>
2022-01-23 22:50:16 +0800

    first.commit

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,6 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("baihezi");
-
+        System.out.println("");
+        System.out.println("wode");
     }
 }
